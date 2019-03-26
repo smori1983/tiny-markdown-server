5ce8677023a5d2859857c8e3f1230be62e72ff17
smori <shinichiro.mori.1983@gmail.com>
2019-03-26 23:55:32 +0900

    Display sample markdown file

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const fs = require('fs');
+const md = require('markdown-it')();
+
 const server = require('http').createServer(function(request, response) {
+  let content = fs.readFileSync('./sample01.md');
+  let parsed = md.render(content.toString());
+
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write('Hello world!');
+  response.write(parsed);
   response.end();
 });
 
