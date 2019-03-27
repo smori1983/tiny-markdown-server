a1ef93898c8525b5d72dcef4abf54b5e0a6789fa
smori <shinichiro.mori.1983@gmail.com>
2019-03-28 01:11:42 +0900

    Use fs.statSync()

--- main.js
+++ main.js
@@ -6,19 +6,27 @@ const md = require('markdown-it')();
 const server = require('http').createServer(function(request, response) {
   let file = request.url;
   let path = './files' + file;
+  let responseCode = 200;
   let result;
 
   if (file === '/') {
     result = 'Index';
-  } else if (fs.existsSync(path)) {
-    let content = fs.readFileSync(path);
-
-    result = md.render(content.toString());
   } else {
-    result = 'Not Found.'
+    try {
+      fs.statSync(path);
+
+      let content = fs.readFileSync(path);
+
+      result = md.render(content.toString());
+    } catch (e) {
+      console.log(e);
+
+      responseCode = 404;
+      result = 'Not Found.'
+    }
   }
 
-  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.writeHead(responseCode, { 'Content-Type': 'text/html' });
   response.write(result);
   response.end();
 });
