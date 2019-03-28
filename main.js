990cd25dec3dd7229a2f7730e860657710911047
smori <shinichiro.mori.1983@gmail.com>
2019-03-29 00:48:16 +0900

    makeResponse()

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
 'use strict';
 
-const fs = require('fs');
-const md = require('markdown-it')();
-
-const server = require('http').createServer(function(request, response) {
-  let file = request.url;
-  let path = './files' + file;
-  let responseCode = 200;
-  let result;
-
-  if (file === '/') {
-    result = 'Index';
-  } else {
-    try {
-      fs.statSync(path);
-
-      let content = fs.readFileSync(path);
-
-      result = md.render(content.toString());
-    } catch (e) {
-      console.log(e);
-
-      responseCode = 404;
-      result = 'Not Found.'
-    }
+const makeResponse = function(path) {
+  if (path === '/') {
+    return {
+      responseCode: 200,
+      body: 'Index',
+    };
   }
 
-  response.writeHead(responseCode, { 'Content-Type': 'text/html' });
-  response.write(result);
+  const fs = require('fs');
+  const md = require('markdown-it')();
+
+  try {
+    let file = './files' + path;
+
+    fs.statSync(file);
+
+    return {
+      responseCode: 200,
+      body: md.render(fs.readFileSync(file).toString()),
+    };
+  } catch (e) {
+    console.log(e);
+
+    return {
+      responseCode: 404,
+      body: 'Not Found.',
+    };
+  }
+};
+
+const server = require('http').createServer(function(request, response) {
+  let result = makeResponse(request.url);
+
+  response.writeHead(result.responseCode, { 'Content-Type': 'text/html' });
+  response.write(result.body);
   response.end();
 });
 
