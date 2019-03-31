7443a60a8fccd887d346ed507470e567cb4b76df
smori <shinichiro.mori.1983@gmail.com>
2019-04-01 00:15:23 +0900

    Extract functions from makeResponse()

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const mime = require('mime');
 
 const indexUtil = require('./lib/indexUtil');
@@ -21,38 +22,68 @@ const response404 = function(body) {
   return responseOf(404, mime.getType('html'), body);
 };
 
-const makeResponse = function(path) {
-  const fs = require('fs');
-  const md = require('markdown-it')();
-
+const index = function(path) {
   if (path === '/') {
     return response200(ejsUtil.render('index.ejs', {
       files: indexUtil.scanMarkdownFiles('./files'),
     }));
   }
+};
 
-  let file;
-
-  try {
-    if (/^\/files_builtin\//.test(path)) {
-      file = __dirname + path;
-    } else {
-      file = './files' + path;
-    }
+const buildInFile = function(path) {
+  if (/^\/files_builtin\//.test(path)) {
+    const file = __dirname + path;
 
     fs.statSync(file);
 
-    if (/\.md$/.test(file)) {
-      return response200(ejsUtil.render('markdown.ejs', {
-        content: md.render(fs.readFileSync(file).toString()),
-      }));
-    } else {
-      return response200(fs.readFileSync(file), mime.getType(file));
-    }
-  } catch (e) {
-    console.log(e);
+    return response200(fs.readFileSync(file), mime.getType(file));
+  }
+};
 
-    return response404(ejsUtil.render('404.ejs'));
+const markdown = function(path) {
+  if (/\.md$/.test(path)) {
+    const md = require('markdown-it')();
+    const file = './files' + path;
+
+    fs.statSync(file);
+
+    return response200(ejsUtil.render('markdown.ejs', {
+      content: md.render(fs.readFileSync(file).toString()),
+    }));
+  }
+};
+
+const otherResource = function(path) {
+  const file = './files' + path;
+
+  fs.statSync(file);
+
+  return response200(fs.readFileSync(file), mime.getType(file));
+};
+
+const sentinel = function() {
+  return response404(ejsUtil.render('404.ejs'));
+};
+
+const makeResponse = function(path) {
+  const responseMakers = [
+    index,
+    buildInFile,
+    markdown,
+    otherResource,
+    sentinel,
+  ];
+
+  let result;
+
+  for (let i = 0, len = responseMakers.length; i < len; i++) {
+    try {
+      if (result = responseMakers[i](path)) {
+        return result;
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
 
