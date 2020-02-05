00c8a730de864e04e5ef9b2f64d0dc41c2170d34
smori <shinichiro.mori.1983@gmail.com>
2020-02-05 23:09:02 +0900

    [test] app.validation

---
+++ test/app/validation.js
@@ -0,0 +1,83 @@
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const assert = require('assert');
+const SUT = require('../../app/validation');
+
+describe('app.validation', function () {
+  describe('valid pattern', function () {
+    it('80 is valid for port', function () {
+      const data = {
+        directory: __dirname,
+        port: '80',
+      };
+
+      assert.strictEqual(SUT.execute(data).isValid, true);
+    });
+
+    it('65535 is valid for port', function () {
+      const data = {
+        directory: __dirname,
+        port: '65535',
+      };
+
+      assert.strictEqual(SUT.execute(data).isValid, true);
+    });
+  });
+
+  describe('invalid pattern', function () {
+    it('lacks directory', function () {
+      const data = {
+        port: '80',
+      };
+
+      assert.strictEqual(SUT.execute(data).isValid, false);
+    });
+
+    it('lacks port', function () {
+      const data = {
+        directory: __dirname,
+      };
+
+      const result = SUT.execute(data);
+
+      assert.strictEqual(result.isValid, false);
+      assert.deepStrictEqual(result.errors, ['port']);
+    });
+
+    it('directory does not exist', function () {
+      const data = {
+        directory: '/foo/bar',
+        port: '80',
+      };
+
+      const result = SUT.execute(data);
+
+      assert.strictEqual(result.isValid, false);
+      assert.deepStrictEqual(result.errors, ['directory']);
+    });
+
+    it('79 is invalid for port', function () {
+      const data = {
+        directory: __dirname,
+        port: '79',
+      };
+
+      const result = SUT.execute(data);
+
+      assert.strictEqual(result.isValid, false);
+      assert.deepStrictEqual(result.errors, ['port']);
+    });
+
+    it('65536 is invalid for port', function () {
+      const data = {
+        directory: __dirname,
+        port: '65536',
+      };
+
+      const result = SUT.execute(data);
+
+      assert.strictEqual(result.isValid, false);
+      assert.deepStrictEqual(result.errors, ['port']);
+    });
+  });
+});
