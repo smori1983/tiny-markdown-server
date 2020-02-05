2576e438747b90dc8f418da6b86002df015c1a50
smori <shinichiro.mori.1983@gmail.com>
2020-02-05 23:48:43 +0900

    [test] lib.indexUtil for '.md' files

---
+++ test/lib/indexUtil.js
@@ -0,0 +1,42 @@
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const assert = require('assert');
+const SUT = require('../../lib/indexUtil');
+
+describe('lib.indexUtil', function () {
+  describe('scanMarkdownFiles', function () {
+    it('.md - 1 file found', function () {
+      const dir = __dirname + '/../../test_resource/dir_01';
+
+      const result = SUT.scanMarkdownFiles(dir);
+
+      const expected = [
+        {
+          path: '/file_01.md',
+          notation: 'file_01.md',
+        },
+      ];
+
+      assert.deepStrictEqual(result, expected);
+    });
+
+    it('.md - 2 files found', function () {
+      const dir = __dirname + '/../../test_resource/dir_02';
+
+      const result = SUT.scanMarkdownFiles(dir);
+
+      const expected = [
+        {
+          path: '/file_01.md',
+          notation: 'file_01.md',
+        },
+        {
+          path: '/file_03.md',
+          notation: 'file_03.md',
+        },
+      ];
+
+      assert.deepStrictEqual(result, expected);
+    });
+  });
+});
