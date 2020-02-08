0ecb851ba6e73470626308f4180eabb27d3ccb9e
smori <shinichiro.mori.1983@gmail.com>
2020-02-09 03:05:41 +0900

    [test] Path encoding

--- test/lib/indexUtil.js
+++ test/lib/indexUtil.js
@@ -5,80 +5,141 @@ const SUT = require('../../lib/indexUtil');
 
 describe('lib.indexUtil', function () {
   describe('scanMarkdownFiles', function () {
-    it('.md - 1 file found', function () {
-      const dir = __dirname + '/../../test_resource/dir_01';
+    describe('file extension', function () {
+      it('.md - 1 file found', function () {
+        const dir = __dirname + '/../../test_resource/dir_01';
 
-      const result = SUT.scanMarkdownFiles(dir);
+        const result = SUT.scanMarkdownFiles(dir);
 
-      const expected = [
-        {
-          path: '/file_01.md',
-          notation: 'file_01.md',
-        },
-      ];
+        const expected = [
+          {
+            path: '/file_01.md',
+            notation: 'file_01.md',
+          },
+        ];
 
-      assert.deepStrictEqual(result, expected);
+        assert.deepStrictEqual(result, expected);
+      });
+
+      it('.md - 2 files found', function () {
+        const dir = __dirname + '/../../test_resource/dir_02';
+
+        const result = SUT.scanMarkdownFiles(dir);
+
+        const expected = [
+          {
+            path: '/file_01.md',
+            notation: 'file_01.md',
+          },
+          {
+            path: '/file_03.md',
+            notation: 'file_03.md',
+          },
+        ];
+
+        assert.deepStrictEqual(result, expected);
+      });
+
+      it('.markdown - 1 file found', function () {
+        const dir = __dirname + '/../../test_resource/dir_03';
+
+        const result = SUT.scanMarkdownFiles(dir);
+
+        const expected = [
+          {
+            path: '/file_02.markdown',
+            notation: 'file_02.markdown',
+          },
+        ];
+
+        assert.deepStrictEqual(result, expected);
+      });
+
+      it('has directory - 4 files found', function () {
+        const dir = __dirname + '/../../test_resource/dir_04';
+
+        const result = SUT.scanMarkdownFiles(dir);
+
+        const expected = [
+          {
+            path: '/dir_04_01/file_04.md',
+            notation: 'dir_04_01/file_04.md',
+          },
+          {
+            path: '/dir_04_01/file_05.markdown',
+            notation: 'dir_04_01/file_05.markdown',
+          },
+          {
+            path: '/file_01.md',
+            notation: 'file_01.md',
+          },
+          {
+            path: '/file_03.markdown',
+            notation: 'file_03.markdown',
+          },
+        ];
+
+        assert.deepStrictEqual(result, expected);
+      });
     });
 
-    it('.md - 2 files found', function () {
-      const dir = __dirname + '/../../test_resource/dir_02';
+    describe('encode', function () {
+      it('# should be encoded to %23', function () {
+        const dir = __dirname + '/../../test_resource/dir_05';
 
-      const result = SUT.scanMarkdownFiles(dir);
+        const result = SUT.scanMarkdownFiles(dir);
 
-      const expected = [
-        {
-          path: '/file_01.md',
-          notation: 'file_01.md',
-        },
-        {
-          path: '/file_03.md',
-          notation: 'file_03.md',
-        },
-      ];
+        const expected = [
+          {
+            path: '/dir_05%2301/file%2302.md',
+            notation: 'dir_05#01/file#02.md',
+          },
+          {
+            path: '/file%2301.md',
+            notation: 'file#01.md',
+          },
+        ];
 
-      assert.deepStrictEqual(result, expected);
-    });
+        assert.deepStrictEqual(result, expected);
+      });
 
-    it('.markdown - 1 file found', function () {
-      const dir = __dirname + '/../../test_resource/dir_03';
+      it('+ should be encoded to %2B', function () {
+        const dir = __dirname + '/../../test_resource/dir_06';
 
-      const result = SUT.scanMarkdownFiles(dir);
+        const result = SUT.scanMarkdownFiles(dir);
 
-      const expected = [
-        {
-          path: '/file_02.markdown',
-          notation: 'file_02.markdown',
-        },
-      ];
+        const expected = [
+          {
+            path: '/dir_06%2B01/file%2B02.md',
+            notation: 'dir_06+01/file+02.md',
+          },
+          {
+            path: '/file%2B01.md',
+            notation: 'file+01.md',
+          },
+        ];
 
-      assert.deepStrictEqual(result, expected);
-    });
+        assert.deepStrictEqual(result, expected);
+      });
 
-    it('has directory - 4 files found', function () {
-      const dir = __dirname + '/../../test_resource/dir_04';
+      it('? should be encoded to %3F', function () {
+        const dir = __dirname + '/../../test_resource/dir_07';
 
-      const result = SUT.scanMarkdownFiles(dir);
+        const result = SUT.scanMarkdownFiles(dir);
 
-      const expected = [
-        {
-          path: '/dir_04_01/file_04.md',
-          notation: 'dir_04_01/file_04.md',
-        },
-        {
-          path: '/dir_04_01/file_05.markdown',
-          notation: 'dir_04_01/file_05.markdown',
-        },
-        {
-          path: '/file_01.md',
-          notation: 'file_01.md',
-        },
-        {
-          path: '/file_03.markdown',
-          notation: 'file_03.markdown',
-        },
-      ];
+        const expected = [
+          {
+            path: '/dir_07%3F01/file%3F02.md',
+            notation: 'dir_07?01/file?02.md',
+          },
+          {
+            path: '/file%3F01.md',
+            notation: 'file?01.md',
+          },
+        ];
 
-      assert.deepStrictEqual(result, expected);
+        assert.deepStrictEqual(result, expected);
+      });
     });
   });
 });
