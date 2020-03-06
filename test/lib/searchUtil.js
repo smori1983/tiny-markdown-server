1fa02807b765add6ed496abebe097fe52ba9a5cf
smori <shinichiro.mori.1983@gmail.com>
2020-03-06 09:47:28 +0900

    searchUtil.toWords()

--- test/lib/searchUtil.js
+++ test/lib/searchUtil.js
@@ -4,39 +4,66 @@ const assert = require('assert');
 const SUT = require('../../lib/searchUtil');
 
 describe('lib.searchUtil', function () {
+  describe('toWords', function () {
+    given(
+      ['', 'empty string'],
+      [' ', '1 space'],
+      ['  ', '2 spaces'],
+      ['　', 'Japanese space'],
+    ).it('should return empty array', function (value) {
+      assert.deepStrictEqual(SUT.toWords(value), []);
+    });
+
+    given(
+      ['a ', ['a']],
+      [' a ', ['a']],
+      [' a', ['a']],
+      ['　a　', ['a']],
+    ).it('should return array with 1 element', function (value, result) {
+      assert.deepStrictEqual(SUT.toWords(value), result);
+    });
+
+    given(
+      ['a aa  aaa', ['a', 'aa', 'aaa']],
+      [' a  aa  aaa ', ['a', 'aa', 'aaa']],
+    ).it('should return array with multiple elements', function (value, result) {
+      assert.deepStrictEqual(SUT.toWords(value), result);
+    });
+  });
+
   describe('toRegExp', function () {
-    given([
+    given(
       'hello',
       'Hello',
       'HELLO',
-    ]).it('should match as case-insensitive (HELLO)', function (value) {
+    ).it('should match as case-insensitive (HELLO)', function (value) {
       const regExp = SUT.toRegExp('HELLO');
       assert.ok(regExp.test(value));
     });
 
-    given([
+    given(
       '|',
       '|key|value|',
       '/(a|b)/',
-    ]).it('should match for text contains "|"', function (value) {
+    ).it('should match for text contains "|"', function (value) {
       const regExp = SUT.toRegExp('|');
       assert.ok(regExp.test(value));
     });
 
-    given([
+    given(
       '+',
       '1+1=2',
       '/.+/',
-    ]).it('should match for text contains "+"', function (value) {
+    ).it('should match for text contains "+"', function (value) {
       const regExp = SUT.toRegExp('+');
       assert.ok(regExp.test(value));
     });
 
-    given([
+    given(
       'aaaaa\n00000\n00000',
       '00000\naaaaa\n00000',
       '00000\n00000\naaaaa',
-    ]).it('should match for multiline text', function (value) {
+    ).it('should match for multiline text', function (value) {
       const regExp = SUT.toRegExp('aaaaa');
       assert.ok(regExp.test(value));
     });
