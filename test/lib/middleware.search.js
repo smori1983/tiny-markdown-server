2bea16235e599d06dbbf541562b0aa52ef5c8e4b
smori <shinichiro.mori.1983@gmail.com>
2020-03-07 01:47:57 +0900

    Update middleware.search.js with searchUtil.toWords()

--- test/lib/middleware.search.js
+++ test/lib/middleware.search.js
@@ -48,4 +48,37 @@ describe('middleware.search', function () {
     assert.strictEqual(json[0].notation, 'file_01.md');
     assert.strictEqual(next.notCalled, true);
   });
+
+  it('should not response any result for empty query', function () {
+    const dir = __dirname + '/../../test_resource/search_01';
+
+    const req = {method: 'GET', query: {word: ''}};
+    const res = {json: sinon.spy()};
+    const next = sinon.spy();
+
+    SUT(dir)(req, res, next);
+
+    /** @type {IndexItem[]} */
+    const json = res.json.getCall(0).args[0];
+
+    assert.strictEqual(json.length, 0);
+  });
+
+  it('should handle multiple words', function () {
+    const dir = __dirname + '/../../test_resource/search_01';
+
+    const req = {method: 'GET', query: {word: '01 02'}};
+    const res = {json: sinon.spy()};
+    const next = sinon.spy();
+
+    SUT(dir)(req, res, next);
+
+    /** @type {IndexItem[]} */
+    const json = res.json.getCall(0).args[0];
+
+    assert.strictEqual(json.length, 2);
+    assert.strictEqual(json[0].notation, 'file_01.md');
+    assert.strictEqual(json[1].notation, 'file_02.md');
+    assert.strictEqual(next.notCalled, true);
+  });
 });
